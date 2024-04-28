a571f9b54bed701aadcddd3a7918fee1bcd0e587
Mizab <mizabmansari@gmail.com>
2024-04-28 18:16:35 +0530

    Updated the imports

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,8 @@
 import { MCFunction, Objective, seed } from "sandstone";
-import { rng } from "./rng";
+import { rng } from "./lcgRng";
 
 MCFunction("test2233", () => {
-    const test_rng = Objective.create("test_rng", "dummy");
-    const output = test_rng("output_test_rng");
-    output.set(rng(0, 10));
-})
+  const test_rng = Objective.create("test_rng", "dummy");
+  const output = test_rng("output_test_rng");
+  output.set(rng(0, 10));
+});
